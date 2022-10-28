f1d569f3b58d19c4a4bec50e0dd383523972f9db
Jenni Heloaho <jenni.heloaho@gmail.com>
2022-10-28 12:15:22 +0300

    strtrim fix

--- part2/ft_strtrim.c
+++ part2/ft_strtrim.c
@@ -1,17 +1,54 @@
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+static int	ft_getstart(const char *s1, const char *set)
 {
-	char	*trim;
-	int	i;
+	size_t	len;
+	size_t	i;
 
+	len = ft_strlen(s1);
 	i = 0;
-	if (!s1)
-		return (NULL);
-
-	*trim = ft_substr(s1, set, (ft_strlen(s1) - set));
-	if (trim == NULL)
-		return (NULL);
-	return (trim);
+	while (i < len)
+	{
+		if (ft_strchr(set, s1[i]) == 0)
+			break ;
+		i++;
+	}
+	return (i);
 }
 
+static int	ft_getend(const char *s1, const char *set)
+{
+	size_t	len;
+	size_t	i;
+
+	len = ft_strlen(s1);
+	i = 0:
+	while (i < len)
+	{
+		if (ft_strchr(set, s1[len - i - 1]) == 0)
+			break ;
+		i++;
+	}
+	return (len - i);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	char	*trim;
+
+	if (s1 == NULL)
+		return (NULL);
+	if (set == NULL)
+		return (ft_strdup(s1));
+	start = ft_getstart(s1, set);
+	end = ft_getend(s1, set);
+	if (start >= end)
+		return (ft_strdup(""));
+	trim = (char *) malloc(sizeof(char) * (end - start + 1))<;
+	if (trim == NULL)
+		return (NULL);
+	ft_strlcpy(trim, (s1 + start), (end - start + 1));
+	return (trim);
+}
