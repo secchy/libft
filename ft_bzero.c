6298576baa537613a07293e352c5ef2dd0d2c113
Jenni Heloaho <jenni.heloaho@gmail.com>
2022-10-28 18:10:08 +0300

    bezero -> bzero

--- ft_bzero.c
+++ ft_bzero.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jheloaho <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jheloaho <jheloaho@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 13:58:12 by jheloaho          #+#    #+#             */
-/*   Updated: 2022/10/28 13:58:13 by jheloaho         ###   ########.fr       */
+/*   Updated: 2022/10/28 18:09:39 by jheloaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_bezero(void *s, size_t len)
+void	ft_bzero(void *s, size_t len)
 {
 	ft_memset(s, '\0', len);
 }
