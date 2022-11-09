48fe37a68f2f770223e5ed581e1058f7ca75a292
Jenni Heloaho <jenni.heloaho@gmail.com>
2022-11-09 20:34:05 +0200

    fixed ft_lstmap, now works if only 2 nodes in list

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: jheloaho <jheloaho@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 16:03:52 by jheloaho          #+#    #+#             */
-/*   Updated: 2022/11/09 20:05:25 by jheloaho         ###   ########.fr       */
+/*   Updated: 2022/11/09 20:29:10 by jheloaho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,20 @@
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*head;
+	t_list	*tmp;
 
 	if (!lst || !f || !del)
 		return (0);
 	new = ft_lstnew(f(lst->content));
 	if (!new)
 		return (NULL);
-	head = new;
-	lst = lst->next;
 	while (lst->next != NULL)
 	{
-		new->next = ft_lstnew(f(lst->content));
-		if (!new->next)
-		{
-			ft_lstclear(&head, del);
-			return (0);
-		}
-		new = new->next;
 		lst = lst->next;
+		tmp = ft_lstnew(f(lst->content));
+		if (!tmp)
+			ft_lstclear(&new, del);
+		ft_lstadd_back(&new, tmp);
 	}
-	new->next = ft_lstnew(f(lst->content));
-	new = new->next;
-	new->next = NULL;
-	return (head);
+	return (new);
 }
